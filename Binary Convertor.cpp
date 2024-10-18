7bcbb48fb56f979c5797462b8edd5914e4322763
Raja Muhammad Bilal Arshad <bilall.dude@gmail.com>
2024-10-19 02:12:30 +0500

    Create Binary Convertor.cpp

---
+++ Binary Convertor.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter your number: ";
+    cin>>a;
+    
+    int count=0,rem,result=0;
+    while(a!=0)
+    {
+        rem=a%10;
+        if(rem!=1 && rem!=0)
+    {
+        cout<<"INVALID INPUT";
+        return 1;
+    }
+        a=a/10;
+        result=result+(rem*pow(2,count));
+        count++;
+    }
+    cout<<result<<endl;
+    return 0;
+    system("pause");
+}
